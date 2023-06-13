33ab741dc89a7a16fe72cabe0acbe7d58b404f94
WYZ00 <thanthtettin@gmail.com>
2023-06-13 23:06:04 +0630

    GetAllIncomeReport & GetIncomeReportByID add

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,25 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+
+import { ReportType, data } from 'src/data';
 
 @Controller('report/:type')
 export class AppController {
   @Get()
-  getAllIncomeReport() {
-    return [];
+  getAllIncomeReport(@Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    return data.report.filter((report) => report.type === reportType);
   }
 
   @Get(':id')
-  getIncomeReportById() {
-    return {};
+  getIncomeReportById(@Param('type') type: string, @Param('id') id: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    return data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
   }
 
   @Post()
