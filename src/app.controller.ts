fd8cc3fe66aeeddef654611f4e44dab28e0a8b1e
WYZ00 <thanthtettin@gmail.com>
2023-06-17 17:37:32 +0630

    Add Summary and done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('report/:type')
-export class AppController {}
+@Controller()
+export class AppController {
+  @Get()
+  gethello() {
+    return 'Welcome Expense App';
+  }
+}
